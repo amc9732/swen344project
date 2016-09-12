9f7ee19a09178c253eab46195597a7f68b309692
Tyler Russell <tlr3552@rit.edu>
2016-09-12 17:16:18 -0400

    app folder and .jshintrc added

---
+++ app/scripts/controllers/WelcomeCtrl.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var WelcomeCtrl = function($scope) {
+  $scope.testVar = 'We are up and running!';
+};
+
+module.exports = WelcomeCtrl;
---
+++ app/spec/helloworld.spec.js
@@ -0,0 +1,7 @@
+//Example Jasmine test. Once there are controllers, we can 
+//instantiate instances of them here, then write real tests.
+describe("Hello World", function() {
+    it("equals Hello World", function() {
+        expect("Hello World").toBe("Hello World");
+    });
+});
