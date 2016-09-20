4e03b4c35cff446ea85666adb386a5ea49ecda92
Tyler Russell <tlr3552@rit.edu>
2016-09-20 13:32:53 -0400

    more deployment work, and provisioned MySQL database through heroku.

--- app.js
+++ app.js
@@ -10,13 +10,54 @@ var users = require('./routes/users');
 
 var app = express();
 
+
+
+
+
+var mysql = require("mysql");
+
+// First you need to create a connection to the db
+var con = mysql.createConnection({
+  host: "us-cdbr-iron-east-04.cleardb.net",
+  user: "b421d9d0efc603",
+  password: "f96ce1ba",
+  database: "heroku_a783285fe27a1fb"
+});
+
+con.connect(function(err){
+  if(err){
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection successfully established.');
+});
+
+//This is an exampl,e query for the MySQL database, and should be removed once we have a real app going.
+//To connect to it just run "node app.js".
+var v = { f_name: 'Tyler' };
+con.query('INSERT INTO user_profile SET ?', v, function(err,res){
+  if(err) throw err;
+  console.log('ID for inserted user:', res.insertId);
+});
+
+con.end(function(err) {
+  // The connection is terminated gracefully
+  // Ensures all previously enqueued queries are still
+  // before sending a COM_QUIT packet to the MySQL server.
+});
+
+
+
+
+
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'html');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -32,8 +73,6 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
-// error handlers
 
 // development error handler
 // will print stacktrace
