9ebc9e8f6c2832a97d4bea082ac9004f6dc1be1c
Tyler Russell <tlr3552@rit.edu>
2016-10-19 15:00:21 -0400

    revert example API back to working one, and removed the example API unit test that didnt work

--- app/js/APIController.js
+++ app/js/APIController.js
@@ -1,7 +1,7 @@
 angular.module('ExampleAPIModule', [])
 
 	.controller('ExampleAPIController', function($scope, $http) {
-	    var api = 'http://vm344e.se.rit.edu/api/Student.php?action=get_student_by_id&id=1';
+	    var api = 'https://jsonplaceholder.typicode.com/posts/1';
 	    $http.get(api).
 	        then(function(response) {
 	            $scope.json = response.data;
