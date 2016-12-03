5ea2e0d7dfb08c1a034e03e6395a7ea1ff1acb4e
Tyler Russell <tlr3552@rit.edu>
2016-12-03 13:03:54 -0500

    minor auth changes, still not done. major issue that I cannot fix noted in comments in login controller

--- app/js/login.js
+++ app/js/login.js
@@ -2,12 +2,29 @@ var oauthModule = angular.module('GoogleOAuthModule', ['ngRoute']);
 	
 oauthModule
 	
-	.controller('LoginController', function ($location, $scope, $timeout, $route) {
+	.controller('LoginController', function ($location, $scope, $timeout, $route, $http) {
 
 		$scope.loggedIn = null;
 
 		$scope.$on('event:google-plus-signin-success', function (event, authResult) {
 			// User successfully authorized the G+ App!
+			var token = authResult.access_token;
+			localStorage.setItem('access_token', token);
+
+			//Major issue below that isnt resolved yet!!!!
+			//Also need to check if the user is already in the system, and if not, redirect to 
+			//http://vm344b.se.rit.edu/auth/google   or the HR teams page
+
+			// $http.get('https://www.googleapis.com/plus/v1/people/me')
+			// 	.success(function(data){
+			// 		$scope.contents = data;
+			// 		console.log(data);
+							
+			// 	})
+			// 	.error(function (error) {
+			// 		console.log(error);
+			// 	});
+
 			$scope.loggedIn = true;
 			$timeout(callAtTimeout, 1000);
 		});
