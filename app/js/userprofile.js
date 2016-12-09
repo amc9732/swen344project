0e834cd8beb496bfc3cc2e800bc3afc78509cd78
Josh James <Josh James>
2016-12-08 20:57:59 -0500

    Get students current courses

--- app/js/userprofile.js
+++ app/js/userprofile.js
@@ -12,7 +12,7 @@ app
     		if(localStorage.getItem('linkedInImageUrl'))
     			return true;
     		return false;
-    	}
+    	};
 
     	//This event is called by the angular-social-login bower package
     	$rootScope.$on('event:social-sign-in-success', function(event, userDetails) {
@@ -28,12 +28,24 @@ app
     	$scope.removeLinkedIn = function() {
     		localStorage.removeItem('linkedInImageUrl');
     		$route.reload();
-    	}
+    	};
 
     	$scope.selectedRow = null;
 		
 		$scope.setClickedRow = function(index) {
 			$scope.selectedRow = index;
-		}
-        
+		};
+
+        $scope.getCurrentCourse = function() {
+            $http.get("http://localhost:1337/vm344e.se.rit.edu/api/StudentClass.php?action=get_classIds_by_student_id&studentid=3")
+                .success(function(data) {
+                    console.log(data);
+                })
+                .error(function(data){
+                    console.log("Error pulling data from student");
+                });
+    	}
+
+    	$scope.getCurrentCourse();
+
     });
